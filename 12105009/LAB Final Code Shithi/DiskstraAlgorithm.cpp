b4517b1092b924ca60f6069d8b43f5e4d9b7bd22
Md. An Nahian Prince <annahian44@gmail.com>
2025-07-29 20:21:56 +0600

    Create DiskstraAlgorithm.cpp

---
+++ 12105009/LAB Final Code Shithi/DiskstraAlgorithm.cpp
@@ -0,0 +1,128 @@
+/*
+Dijkstra's Algorithm
+Single Pair Shortest Path
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MX 105
+#define INF 1e8
+
+struct node
+{
+    int node;
+    int cost;
+};
+
+vector<node> Graph[MX];
+bool vis[MX];
+int dist[MX];
+
+void reset()
+{
+    for (int i = 0; i < MX; i++)
+    {
+        Graph[i].clear();
+        vis[i] = 0;
+        dist[i] = INF;
+    }
+}
+
+class cmp
+{
+public:
+    bool operator()(node &A, node &B)
+    {
+        if (A.cost > B.cost)
+        {
+            return true;
+        }
+        return false;
+    }
+};
+
+void dijkstra(int source)
+{
+
+    priority_queue<node, vector<node>, cmp> PQ;
+
+    PQ.push({source, 0});
+
+    while (!PQ.empty())
+    {
+        node current = PQ.top();
+        PQ.pop();
+
+        int node = current.node;
+
+        int cost = current.cost;
+
+        if (vis[node] == 1)
+        {
+            continue;
+        }
+
+        dist[node] = cost;
+        vis[node] = 1;
+
+        for (int i = 0; i < Graph[node].size(); i++)
+        {
+            int nxtNode = Graph[node][i].node;
+
+            int nxtCost = Graph[node][i].cost;
+
+            if (vis[nxtNode] == 0)
+            {
+                PQ.push({nxtNode, cost + nxtCost});
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    reset();
+
+    int nodes, edges;
+    cin >> nodes >> edges;
+
+    for (int i = 1; i <= edges; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        Graph[u].push_back({v, w});
+    }
+
+    int source;
+    cin >> source;
+
+    dijkstra(source);
+
+    for (int i = 1; i <= nodes; i++)
+    {
+        cout << "Node: " << i << " Distance: ";
+        if (dist[i] == INF)
+        {
+            cout << "INF" << "\n";
+        }
+        else
+        {
+            cout << dist[i] << "\n";
+        }
+    }
+
+    return 0;
+}
+
+/*t-1
+5 6
+1 2 2
+1 3 1
+2 3 1
+1 4 3
+4 5 2
+5 3 5
+1
+*/
